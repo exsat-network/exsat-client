8e5f08b0a28355b91d5e02a8484b0cacc5f9fc55
chenm <chenminmin4@hotmail.com>
2024-09-04 11:32:28 +0800

    Code formatting

--- src/utils/input.ts
+++ src/utils/input.ts
@@ -1,24 +1,24 @@
-import {input} from "@inquirer/prompts";
+import { input } from "@inquirer/prompts";
 
 export async function inputWithCancel(
-    message: string,
-    validatefn?: (value: string) => boolean | string | Promise<string | boolean>,
+  message: string,
+  validatefn?: (value: string) => boolean | string | Promise<string | boolean>,
 ) {
-    let value = await input({
-        message: message,
-        validate: (input) => {
-            if (input.trim().toLowerCase() === 'q') {
-                return true;
-            }
-            if (typeof validatefn === 'function') {
-                return validatefn(input);
-            }
-            return true;
-        },
-    });
-    value = value.trim();
-    if (value.toLowerCase() === 'q') {
-        return false;
-    }
-    return value;
+  let value = await input({
+    message: message,
+    validate: (input) => {
+      if (input.trim().toLowerCase() === 'q') {
+        return true;
+      }
+      if (typeof validatefn === 'function') {
+        return validatefn(input);
+      }
+      return true;
+    },
+  });
+  value = value.trim();
+  if (value.toLowerCase() === 'q') {
+    return false;
+  }
+  return value;
 }
