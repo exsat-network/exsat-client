a7424c7d0fe4da3befa8e6aab7d27e68422df09b
RobinHolmes <robin@exsat.org>
2024-09-10 23:42:08 +0800

    refactor: client jobs

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,181 +1,34 @@
 import cron from 'node-cron';
+import { getAccountInfo, getConfigPassword, getInputPassword } from '../utils/keystore';
+import { configureLogger, logger } from '../utils/logger';
+import { envCheck } from '../utils/common';
+import ExsatApi from '../utils/exsat-api';
+import TableApi from '../utils/table-api';
+import { Client, ClientType } from '../utils/enumeration';
+import { errorTotalCounter, setupPrometheus, warnTotalCounter } from '../utils/prom';
+import { ValidatorJobs } from './jobs';
+import { AsyncLock } from '../utils/asyncLock';
 import {
-  EXSAT_RPC_URLS, PROMETHEUS, PROMETHEUS_ADDRESS,
+  EXSAT_RPC_URLS,
   VALIDATOR_JOBS_ENDORSE,
   VALIDATOR_JOBS_ENDORSE_CHECK,
   VALIDATOR_KEYSTORE_FILE
 } from '../utils/config';
-import { getAccountInfo, getConfigPassword, getInputPassword } from '../utils/keystore';
-import { getblockcount, getblockhash } from '../utils/bitcoin';
-import { configureLogger, logger } from '../utils/logger';
-import { envCheck, getErrorMessage, sleep } from '../utils/common';
-import ExsatApi from '../utils/exsat-api';
-import TableApi from '../utils/table-api';
-import { ClientType, ContractName, ErrorCode } from '../utils/enumeration';
-import {
-  setupPrometheus,
-  errorTotalCounter,
-  warnTotalCounter,
-  blockValidateTotalCounter,
-  validateLatestBlockGauge,
-  validateLatestTimeGauge
-} from '../utils/prom';
 
-// Global variables to track job status and store API instances
-let [endorseRunning, endorseCheckRunning, startupStatus] = [false, false, false];
-let accountName: string;
-let exsatApi: ExsatApi;
-let tableApi: TableApi;
-let lastEndorseHeight: number = 0;
-
-const endorseOperations = {
-  // Check if the account is qualified to endorse
-  isEndorserQualified(endorsers: {
-    account: string
-    staking: number
-  }[], accountName: string): boolean {
-    return endorsers.some(endorser => endorser.account === accountName);
-  },
-
-  // Check if an endorsement is needed and submit if necessary
-  async checkAndSubmit(accountName: string, height: number, hash: string) {
-    const endorsement = await tableApi.getEndorsementByBlockId(height, hash);
-    if (endorsement) {
-      let isQualified = this.isEndorserQualified(endorsement.requested_validators, accountName);
-      if (isQualified && !this.isEndorserQualified(endorsement.provider_validators, accountName)) {
-        await this.submit(accountName, height, hash);
-      } else {
-        lastEndorseHeight = height;
-      }
-    } else {
-      await this.submit(accountName, height, hash);
-    }
-  },
-
-  // Submit an endorsement to the blockchain
-  async submit(validator: string, height: number, hash: string) {
-    const result: any = await exsatApi.executeAction(ContractName.blkendt, 'endorse', { validator, height, hash });
-    if (result && result.transaction_id) {
-      lastEndorseHeight = height;
-      blockValidateTotalCounter.inc({ account: accountName, client: 'validator' });
-      validateLatestBlockGauge.set({ account: accountName, client: 'validator' }, height);
-      validateLatestTimeGauge.set({ account: accountName, client: 'validator' }, Date.now());
-      logger.info(`Submit endorsement success, accountName: ${validator}, height: ${height}, hash: ${hash}, transaction_id: ${result?.transaction_id}`);
-    }
-  },
-};
-
-const jobs = {
-  async endorse() {
-    if (!startupStatus) {
-      startupStatus = await tableApi.getStartupStatus();
-      if (!startupStatus) {
-        logger.info('The exSat Network has not officially launched yet. Please wait for it to start.');
-        return;
-      }
-    }
-    try {
-      if (endorseRunning) {
-        logger.info('Endorse task is already running. Skipping this round.');
-        return;
-      }
-      endorseRunning = true;
-      logger.info('Endorse task is running.');
-      const blockcountInfo = await getblockcount();
-      const blockhashInfo = await getblockhash(blockcountInfo.result);
-      await endorseOperations.checkAndSubmit(accountName, blockcountInfo.result, blockhashInfo.result);
-    } catch (e: any) {
-      const errorMessage = getErrorMessage(e);
-      logger.info(`Endorse task info: ${errorMessage}`);
-      if (errorMessage.startsWith(ErrorCode.Code1001) || errorMessage.startsWith(ErrorCode.Code1003)) {
-        // ignore
-      } else {
-        logger.error('Endorse task error', e);
-        errorTotalCounter.inc({ account: accountName, client: 'validator' });
-      }
-    } finally {
-      endorseRunning = false;
-    }
-  },
-
-  async endorseCheck() {
-    if (!startupStatus) {
-      startupStatus = await tableApi.getStartupStatus();
-      if (!startupStatus) {
-        logger.info('The exSat Network has not officially launched yet. Please wait for it to start.');
-        return;
-      }
-    }
-    if (endorseCheckRunning) {
-      return;
-    }
-    endorseCheckRunning = true;
-    try {
-      logger.info('Endorse check task is running.');
-      const chainstate = await tableApi.getChainstate();
-      if (!chainstate) {
-        logger.error('Get chainstate error.');
-        errorTotalCounter.inc({ account: accountName, client: 'validator' });
-        return;
-      }
-
-      const blockcount = await getblockcount();
-      let startEndorseHeight = chainstate.irreversible_height + 1;
-      if (lastEndorseHeight > startEndorseHeight && lastEndorseHeight < blockcount.result - 6) {
-        startEndorseHeight = lastEndorseHeight;
-      }
-      for (let i = startEndorseHeight; i <= blockcount.result; i++) {
-        let hash: string;
-        try {
-          const blockhash = await getblockhash(i);
-          hash = blockhash.result;
-          logger.info(`Check endorsement for block ${i}/${blockcount.result}`);
-          await endorseOperations.checkAndSubmit(accountName, i, blockhash.result);
-        } catch (e: any) {
-          const errorMessage = getErrorMessage(e);
-          logger.info(`Endorse check task result, height: ${i}, hash: ${hash}, ${errorMessage}`);
-          if (errorMessage.startsWith(ErrorCode.Code1002)) {
-          } else if (errorMessage.startsWith(ErrorCode.Code1001) || errorMessage.startsWith(ErrorCode.Code1003)) {
-            return;
-          } else {
-            logger.error(`Submit endorsement failed, height: ${i}, hash: ${hash}`, e);
-            errorTotalCounter.inc({ account: accountName, client: 'validator' });
-          }
-        }
-      }
-    } catch (e) {
-      logger.error('Endorse check task error', e);
-      errorTotalCounter.inc({ account: accountName, client: 'validator' });
-      await sleep();
-    } finally {
-      endorseCheckRunning = false;
-    }
-  }
-};
-
-// Set up cron jobs for endorsing and checking endorsements
-function setupCronJobs() {
-  const cronJobs = [
-    { schedule: VALIDATOR_JOBS_ENDORSE, job: jobs.endorse },
-    { schedule: VALIDATOR_JOBS_ENDORSE_CHECK, job: jobs.endorseCheck },
-  ];
-
-  cronJobs.forEach(({ schedule, job }) => {
-    cron.schedule(schedule, () => {
-      job().catch(error => {
-        console.error(`Unhandled error in ${job.name} job:`, error);
-        errorTotalCounter.inc({ account: accountName, client: 'validator' });
-      });
-    });
-  });
+export class ValidatorState {
+  accountName: string = '';
+  exsatApi: ExsatApi | null = null;
+  tableApi: TableApi | null = null;
+  lastEndorseHeight: number = 0;
+  startupStatus: boolean = false;
+  endorseLock = new AsyncLock();
+  endorseCheckLock = new AsyncLock();
 }
 
-// Initialize the main application
-async function main() {
-  configureLogger('validator');
-  await envCheck(VALIDATOR_KEYSTORE_FILE);
+async function initializeAccount(): Promise<{ accountInfo: any, password: string }> {
   let password = getConfigPassword(ClientType.Validator);
   let accountInfo;
+
   if (password) {
     password = password.trim();
     accountInfo = await getAccountInfo(VALIDATOR_KEYSTORE_FILE, password);
@@ -189,23 +42,61 @@ async function main() {
         accountInfo = await getAccountInfo(VALIDATOR_KEYSTORE_FILE, password);
       } catch (e) {
         logger.warn(e);
-        warnTotalCounter.inc({ account: accountName, client: 'validator' });
+        warnTotalCounter.inc({ account: accountInfo?.accountName, client: Client.Validator });
       }
     }
   }
-  accountName = accountInfo.accountName;
-  exsatApi = new ExsatApi(accountInfo, EXSAT_RPC_URLS);
-  await exsatApi.initialize();
-  tableApi = new TableApi(exsatApi);
-  await exsatApi.checkClient(ClientType.Validator);
+
+  return { accountInfo, password };
 }
 
-// Entry point of the application
+async function setupApis(accountInfo: any): Promise<{ exsatApi: ExsatApi, tableApi: TableApi }> {
+  const exsatApi = new ExsatApi(accountInfo, EXSAT_RPC_URLS);
+  await exsatApi.initialize();
+  const tableApi = new TableApi(exsatApi);
+  await exsatApi.checkClient(ClientType.Validator);
+  return { exsatApi, tableApi };
+}
+
+function setupCronJobs(jobs: ValidatorJobs) {
+  const cronJobs = [
+    { schedule: VALIDATOR_JOBS_ENDORSE, job: jobs.endorse },
+    { schedule: VALIDATOR_JOBS_ENDORSE_CHECK, job: jobs.endorseCheck }
+  ];
+
+  cronJobs.forEach(({ schedule, job }) => {
+    cron.schedule(schedule, async () => {
+      try {
+        await job();
+      } catch (error) {
+        logger.error(`Unhandled error in ${job.name} job:`, error);
+        errorTotalCounter.inc({ account: jobs.state.accountName, client: Client.Validator });
+      }
+    });
+  });
+}
+
+async function main() {
+  configureLogger(Client.Validator);
+  await envCheck(VALIDATOR_KEYSTORE_FILE);
+
+  const { accountInfo } = await initializeAccount();
+  const { exsatApi, tableApi } = await setupApis(accountInfo);
+
+  const state = new ValidatorState();
+  state.accountName = accountInfo.accountName;
+  state.exsatApi = exsatApi;
+  state.tableApi = tableApi;
+
+  const jobs = new ValidatorJobs(state);
+
+  setupCronJobs(jobs);
+  setupPrometheus();
+}
+
 (async () => {
   try {
     await main();
-    setupCronJobs();
-    setupPrometheus();
   } catch (e) {
     logger.error(e);
   }
---
+++ src/validator/jobs.ts
@@ -0,0 +1,128 @@
+import { logger } from '../utils/logger';
+import { getErrorMessage, sleep } from '../utils/common';
+import { Client, ContractName, ErrorCode } from '../utils/enumeration';
+import {
+  blockValidateTotalCounter,
+  errorTotalCounter,
+  validateLatestBlockGauge,
+  validateLatestTimeGauge
+} from '../utils/prom';
+import { ValidatorState } from './index';
+import { getblockcount, getblockhash } from '../utils/bitcoin';
+
+export class ValidatorJobs {
+  constructor(public state: ValidatorState) {
+  }
+
+  // Check if the account is qualified to endorse
+  isEndorserQualified(endorsers: {
+    account: string
+    staking: number
+  }[], accountName: string): boolean {
+    return endorsers.some(endorser => endorser.account === accountName);
+  }
+
+  // Check if an endorsement is needed and submit if necessary
+  async checkAndSubmit(accountName: string, height: number, hash: string) {
+    const endorsement = await this.state.tableApi!.getEndorsementByBlockId(height, hash);
+    if (endorsement) {
+      let isQualified = this.isEndorserQualified(endorsement.requested_validators, accountName);
+      if (isQualified && !this.isEndorserQualified(endorsement.provider_validators, accountName)) {
+        await this.submit(accountName, height, hash);
+      } else {
+        this.state.lastEndorseHeight = height;
+      }
+    } else {
+      await this.submit(accountName, height, hash);
+    }
+  }
+
+  // Submit an endorsement to the blockchain
+  async submit(validator: string, height: number, hash: string) {
+    const result: any = await this.state.exsatApi!.executeAction(ContractName.blkendt, 'endorse', {
+      validator,
+      height,
+      hash
+    });
+    if (result && result.transaction_id) {
+      this.state.lastEndorseHeight = height;
+      blockValidateTotalCounter.inc({ account: this.state.accountName, client: Client.Validator });
+      validateLatestBlockGauge.set({ account: this.state.accountName, client: Client.Validator }, height);
+      validateLatestTimeGauge.set({ account: this.state.accountName, client: Client.Validator }, Date.now());
+      logger.info(`Submit endorsement success, accountName: ${validator}, height: ${height}, hash: ${hash}, transaction_id: ${result?.transaction_id}`);
+    }
+  }
+
+  endorse = async () => {
+    await this.state.endorseLock.acquire();
+    try {
+      if (!this.state.startupStatus) {
+        this.state.startupStatus = await this.state.tableApi!.getStartupStatus();
+        if (!this.state.startupStatus) {
+          logger.info('The exSat Network has not officially launched yet. Please wait for it to start.');
+          await sleep(60000);
+          return;
+        }
+      }
+      logger.info('Endorse task is running.');
+      const blockcountInfo = await getblockcount();
+      const blockhashInfo = await getblockhash(blockcountInfo.result);
+      await this.checkAndSubmit(this.state.accountName, blockcountInfo.result, blockhashInfo.result);
+    } catch (e) {
+      const errorMessage = getErrorMessage(e);
+      logger.info(`Endorse task info: ${errorMessage}`);
+      if (errorMessage.startsWith(ErrorCode.Code1001) || errorMessage.startsWith(ErrorCode.Code1003)) {
+        // ignore
+      } else {
+        logger.error('Endorse task error', e);
+        errorTotalCounter.inc({ account: this.state.accountName, client: Client.Validator });
+      }
+    } finally {
+      this.state.endorseLock.release();
+    }
+  };
+
+  endorseCheck = async () => {
+    await this.state.endorseCheckLock.acquire();
+    try {
+      logger.info('Endorse check task is running.');
+      const chainstate = await this.state.tableApi!.getChainstate();
+      if (!chainstate) {
+        logger.error('Get chainstate error.');
+        errorTotalCounter.inc({ account: this.state.accountName, client: Client.Validator });
+        return;
+      }
+
+      const blockcount = await getblockcount();
+      let startEndorseHeight = chainstate.irreversible_height + 1;
+      if (this.state.lastEndorseHeight > startEndorseHeight && this.state.lastEndorseHeight < blockcount.result - 6) {
+        startEndorseHeight = this.state.lastEndorseHeight;
+      }
+      for (let i = startEndorseHeight; i <= blockcount.result; i++) {
+        let hash: string;
+        try {
+          const blockhash = await getblockhash(i);
+          hash = blockhash.result;
+          logger.info(`Check endorsement for block ${i}/${blockcount.result}`);
+          await this.checkAndSubmit(this.state.accountName, i, blockhash.result);
+        } catch (e: any) {
+          const errorMessage = getErrorMessage(e);
+          logger.info(`Endorse check task result, height: ${i}, hash: ${hash}, ${errorMessage}`);
+          if (errorMessage.startsWith(ErrorCode.Code1002)) {
+          } else if (errorMessage.startsWith(ErrorCode.Code1001) || errorMessage.startsWith(ErrorCode.Code1003)) {
+            return;
+          } else {
+            logger.error(`Submit endorsement failed, height: ${i}, hash: ${hash}`, e);
+            errorTotalCounter.inc({ account: this.state.accountName, client: Client.Validator });
+          }
+        }
+      }
+    } catch (e) {
+      logger.error('Endorse check task error', e);
+      errorTotalCounter.inc({ account: this.state.accountName, client: Client.Validator });
+      await sleep();
+    } finally {
+      this.state.endorseCheckLock.release();
+    }
+  };
+}
