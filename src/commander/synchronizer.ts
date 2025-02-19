aff8ff85693b2937cf99e70f673b143d4705cc9b
frankoslive <frank@exsat.org>
2025-02-19 14:33:34 +0800

    change network config key

--- src/commander/synchronizer.ts
+++ src/commander/synchronizer.ts
@@ -296,7 +296,7 @@ export class SynchronizerCommander {
       return true;
     } else {
       console.log(
-        `In order to activate your account, please contact our admin via email (${Font.fgCyan}${Font.bright}${NETWORK_CONFIG.syncRegEmail}${Font.reset}).\n`
+        `In order to activate your account, please contact our admin via email (${Font.fgCyan}${Font.bright}${NETWORK_CONFIG.contact}${Font.reset}).\n`
       );
       process.exit(0);
     }
